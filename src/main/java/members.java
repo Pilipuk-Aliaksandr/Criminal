56d6f59633065bf1a0e57581e2e8b0042dec9625
Pilipuk-Aliaksandr <shurapilipuk@gmail.com>
2022-10-24 11:32:17 +0300

    check pr

--- src/main/java/members.java
+++ src/main/java/members.java
@@ -1,2 +1,20 @@
 public class members {
+    private long id;
+    private long user_id;
+    private members(long id, long user_id) {
+        this.user_id=user_id;
+        this.id=id;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public long getUser_id() {
+        return user_id;
+    }
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
 }
